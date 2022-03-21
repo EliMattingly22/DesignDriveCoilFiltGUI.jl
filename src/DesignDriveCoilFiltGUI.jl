e3432a1a1e00d4cc7fd983d0a9c1c01c72d46419
Jonas Schumacher <github735@jonasschumacher.de>
2022-03-21 13:38:15 +0100

    First go on fixing package issues

--- src/DesignDriveCoilFiltGUI.jl
+++ src/DesignDriveCoilFiltGUI.jl
@@ -1,26 +1,15 @@
-module DesignDriveCoilFilt
+module DesignDriveCoilFiltGUI
 using FFTW,Gtk,PyPlot,Optim
 using LinearAlgebra
 using PyPlot
 using CSV
+using Reexport
+
+@reexport using DesignDriveCoilFilt
 
 # using Interpolations
 
-include("Load_LTSpice_Net.jl")
-include("RunAnalysis.jl")
-include("SPICE2Matrix.jl")
-include("ToroidOptimizer.jl")
-include("Filter_Designer.jl")
-include("ThermalModeling.jl")
-include("WriteLTSPICEFile.jl")
 include("Filter_Designer_GUI.jl")
-export DesignDriveFilter_OptimDrift,
-       DesignDriveFilter,
-       ToroidOptimizer,
-       PipeFlow,
-       findResPair,
-       useFilterDesignGUI
-       
-# Write your package code here.
+export useFilterDesignGUI
 
 end
